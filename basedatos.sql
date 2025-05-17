ae3e97a4c6ba6ccaed90826d4112bc64af1a9018
unknown <pablofierrovallejos@hotmail.com>
2025-05-16 21:50:51 -0400

    update basedatos config

--- basedatos.sql
+++ basedatos.sql
@@ -14,3 +14,23 @@ CREATE TABLE `mitrack` (
   PRIMARY KEY (`idmitrack`),
   KEY `idx_fechahora` (`fechahora`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3014 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+Para conectar la base de datos mysql
+1.- En:
+  Crear el archivo C:\Program Files\MySQL\MySQL Server 9.32\my.ini
+  con:
+    [mysqld]
+    bind-address = 0.0.0.0
+
+2.- Reiniciar servicio mysql
+
+3.- Desde:
+  C:\Program Files\MySQL\MySQL Server 9.32\bin
+  Ejecutar:
+    mysql -u root -p
+    mysql> update user set Host='%' where User='root'
+    mysql> flush privileges;
+    mysql> SELECT User, Host FROM mysql.user;
+    mysql> exit
+    
